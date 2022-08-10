202bfbb4193a5e0568843ef5d1c2a5169e9989c6
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-08-10 23:34:35 +0300

    add components files and folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Loader from './Loader/Loader'
+
+const KEY = "9c85c2414d8624d6e18c2da3e97d3f7e";
 export const App = () => {
+  const [movies, setMovies] = useState([]);
+
+    const fetchMovie = async (search, page = 1) => {
+    const result = await axios.get(
+      // `https://pixabay.com/api/?q=${search}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${itemsPerPage}`
+      `https://api.themoviedb.org/3/movie/550?api_key=${KEY}`
+    )
+    return { movies: result.data.hits, totalImages: result.data.totalHits };
+  };
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      Good job!
+    <div>
+    
     </div>
   );
 };
